0c1870cd98e34b85e014eb61411afcbf7752a788
Smita <smitan@questk2.com>
2025-04-22 12:02:17 +0530

    code

---
+++ utils/httpStatusCodes.js
@@ -0,0 +1,25 @@
+// const HttpStatus = {
+//     OK: {
+//       code: 200,
+//       message: 'OK'
+//     },
+//     CREATED: {
+//       code: 201,
+//       message: 'Created'
+//     },
+//     BAD_REQUEST: {
+//       code: 400,
+//       message: 'Bad Request'
+//     },
+//     NOT_FOUND: {
+//       code: 404,
+//       message: 'Not Found'
+//     },
+//     INTERNAL_SERVER_ERROR: {
+//       code: 500,
+//       message: 'Internal Server Error'
+//     }
+//   };
+  
+//   module.exports = HttpStatus;
+  
